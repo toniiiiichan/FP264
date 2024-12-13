9a50122c0dcd37a4d752ea4a76849c57a8aa90df
jel326 <jel326@lehigh.edu>
2024-12-13 18:16:16 -0500

    updated html of Login and Register Page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "app/public/**/*.{html,js,css}",
+    "app/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
